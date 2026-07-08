41a94cda54f2c950411f34f29e3381b8fcc72856
priyacs104 <148523115+priyacs104@users.noreply.github.com>
2026-07-08 08:37:56 +0530

    Add files via upload

---
+++ DAY-3_FUNCTION.py
@@ -0,0 +1,52 @@
+#FUNCTIONS
+
+def hello():
+    print("HELLO")
+
+hello()
+
+
+
+
+def welcome():
+    print("WELCOME")
+
+welcome()
+
+
+
+
+def add(a,b):
+    print(a+b)
+
+add(5,7)
+
+
+#EVEN OR ODD
+
+def even_or_odd(num):
+    if num%2==0:
+        print("Even")
+    else:
+        print("odd")
+
+even_or_odd(8)
+
+
+#square of a number
+
+def square(num):
+    print(num*num)
+
+square(6)
+
+#largest number
+
+def largest(a,b):
+    if a>b:
+        print(a)
+    else:
+        print(b)
+
+largest(100,75)
+
