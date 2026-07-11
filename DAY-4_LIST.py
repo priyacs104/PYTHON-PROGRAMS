54f448f54030de1066f0ba9d21dd8b472fdad51e
priyacs104 <148523115+priyacs104@users.noreply.github.com>
2026-07-11 07:34:18 +0530

    Add files via upload

---
+++ DAY-4_LIST.py
@@ -0,0 +1,54 @@
+#LIST
+
+fruits = ["Apple", "Banana", "Mango"]
+
+print(fruits)
+
+
+fruits = ["Apple", "Banana", "Mango"]
+
+print(fruits[0])
+
+
+fruits = ["Apple", "Banana"]
+
+fruits.append("Orange")
+
+print(fruits)
+
+
+
+fruits = ["Apple", "Banana", "Mango"]
+
+fruits.remove("Banana")
+
+print(fruits)
+
+
+
+numbers = [10, 20, 30, 40]
+
+print(numbers)
+
+
+
+
+numbers = [10, 20, 30, 40]
+
+print(numbers[2])
+
+
+
+names = ["Priya", "Ravi"]
+
+names.append("Kumar")
+
+print(names)
+
+
+
+
+colors = ["Red", "Blue", "Green"]
+
+print(colors[0])
+print(colors[2])
